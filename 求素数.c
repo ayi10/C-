dd91ba740847457b382aa19d6f63aa7c7f4b3cfc
ayi10 <111132950+ayi10@users.noreply.github.com>
2022-08-18 14:13:37 +0800

    Add files via upload

---
+++ 求素数.c
@@ -0,0 +1,18 @@
+//求100到200间的素数
+#include <stdio.h>
+int main()
+{
+	int i = 0;
+	int j = 0;
+	for (i = 100; i <= 200; i++)
+	{
+		for (j = 2;j < i;j++)
+		{
+			if (i % j == 0)
+				break;
+		}
+		if (i ==j)
+			printf("%d\n", i);
+	}
+
+}
