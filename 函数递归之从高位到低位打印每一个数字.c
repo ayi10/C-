201b766ac01d1ad9ea57f6a99dd5325933109957
ayi10 <111132950+ayi10@users.noreply.github.com>
2022-09-07 17:19:50 +0800

    Add files via upload

---
+++ 函数递归之从高位到低位打印每一个数字.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+void print(int i)
+{
+	if (i > 9)
+	{
+		print(i / 10);
+	}
+	printf("%d\n", i%10);
+
+}
+int main()
+{
+	int i = 0;
+	scanf("%d", &i);
+	print(i);
+	return 0;
+}
